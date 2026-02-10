45dff959df35f74b5bee4f147fdc4576e5e5260d
alimurad <amurad@i2cinc.com>
2026-02-10 16:20:34 +0500

    Initial Commit

---
+++ ArabicStories/ArabicStoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArabicStoriesApp.swift
+//  ArabicStories
+//
+//  Created by Ali Murad on 10/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct ArabicStoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArabicStories/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ArabicStories
+//
+//  Created by Ali Murad on 10/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
